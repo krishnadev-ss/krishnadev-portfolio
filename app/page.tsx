5c6498b9ff75a6d6fe23c22c38fc19d42666fbb3
krishnadev-ss <krishnadevsreekumar2002@gmail.com>
2024-06-03 01:54:25 +0530

    bentoGrid

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "@/components/Hero";
 import Image from "next/image";
 import{ FloatingNav }from "@/components/ui/FloatingNav";
 import { FaHome } from "react-icons/fa";
+import Grid from "@/components/Grid";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
           icon: <FaHome />
         }]} />
         <Hero />
+        <Grid />
       </div>
     </main>
   );
